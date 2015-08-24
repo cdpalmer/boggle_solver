d507a272eba7eef31d53341692288d5116e8b302
Cody Palmer <cody.palmer08@gmail.com>
2015-08-23 20:21:51 -0600

    forgot you can go diagonal

--- letter_tile.rb
+++ letter_tile.rb
@@ -1,7 +1,7 @@
 class LetterTile
   attr_accessor :x, :y, :value, :next_letter, :previous_letter
 
-  def initialize(x, y)
+  def initialize(x,y)
     @x = x
     @y = y
     @value = Board.values[x][y]
@@ -21,6 +21,23 @@ class LetterTile
   end
 
   def west_tile
+    return nil if x == 0
+  end
+
+  def northeast_tile
+    return nil if y == 0 && x == Board.height
+    Tile.new(x-1,y-1)
+  end
+
+  def southeast_tile
+    return nil if y == Board.height
+  end
+
+  def southwest_tile
+    return nil if x == Board.height
+  end
+
+  def northwest_tile
     return nil if x == 0
   end
 end
